a5c6b526e53d65499851139472d6b9d0e177c440
brhanuhailu <hailubrhanu29@gmail.com>
2023-08-20 12:27:01 -0700

    components

---
+++ src/components/Greetings.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreetings } from '../redux/messageReducer';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greetings);
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  if (greeting.length === 0) return <h2>loading...</h2>;
+
+  return (
+    <>
+      <div>
+        <h2 style={{ textAlign: 'center', marginTop: '3em' }}>{greeting.greeting}</h2>
+      </div>
+    </>
+  );
+};
+
+export default Greeting;
